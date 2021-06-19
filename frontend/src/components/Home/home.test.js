4315faf75571b55df095c1f817e291cff52c0fde
David Bell <david.jbell@outlook.com>
2021-06-19 13:35:33 +0100

    resend for Will

--- frontend/src/components/Home/home.test.js
+++ frontend/src/components/Home/home.test.js
@@ -5,7 +5,7 @@ import React from 'react';
 import { renderIntoDocument } from 'react-dom/test-utils';
 import '@testing-library/jest-dom/extend-expect';
 
-// Checks the Login button Fires a Fake Function....appears to work only with input to mockSubmitHandler?
+// Checks the Login button Fires a Fake Function....appears to work only with input to mockSubmitHandler
 describe('Test Button component', () => {
   it('should call function on form submit', () => {
     const mockSubmitHandler = jest.fn();
