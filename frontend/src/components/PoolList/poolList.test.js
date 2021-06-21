892dada52b903b8ab5b407df988fd676d512ff39
David Bell <david.jbell@outlook.com>
2021-06-21 18:02:08 +0100

    5 Tests all refactored

--- frontend/src/components/PoolList/poolList.test.js
+++ frontend/src/components/PoolList/poolList.test.js
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import React from 'react';
 import PoolList from '.';
 import { BrowserRouter as Router } from 'react-router-dom';
+
 describe('Tests data Render', () => {
   it('should correctly render mock data to screen', () => {
     const mockData = [
@@ -18,17 +19,5 @@ describe('Tests data Render', () => {
       </Router>
     );
     getByText(mockData[0].nanoId);
-    // const listOutput = document.querySelector('div');
-    // console.log('-----------------------', listOutput);
-    // // <ListItemText primary={pool.nanoId} key={pool.id} />
-    // expect(listOutput).toBe(mockData.nanoId);
   });
 });
-// expect(received).toBe(expected) // Object.is equality
-//     Expected: "TnvJTGu10x"
-//     Received: <div><div /></div>
-
-// Expected: "TnvJTGu10x"
-//     Received: <div><div class="pool-list__container"><div class="pool-list__header"><h5 class="MuiT
-// ypography-root MuiTypography-h5">Active Pools</h5><nav class="MuiList-root pool-list__item MuiList-
-// padding"><div /></nav></div></div></div>
